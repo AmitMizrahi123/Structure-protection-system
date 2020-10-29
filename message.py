7809a762fe27331a4980a024f6e765042d765c60
amit <47501611+AmitMizrahi123@users.noreply.github.com>
2020-10-29 23:54:14 +0200

    Update message.py

--- message.py
+++ message.py
@@ -2,8 +2,8 @@ from twilio.rest import Client
 
 
 def send_message(phone_list):
-    account_sid = 'ACd90b1227023215c5a7c28c27c48a310f'
-    auth_token = 'f09c8d8a1af9514cbf6c4626b9049501'
+    account_sid = '<ACCOUNT SID TWILIO>'
+    auth_token = '<AUTH TOKEN TWILIO>'
     client = Client(account_sid, auth_token)
 
     for phone in phone_list:
