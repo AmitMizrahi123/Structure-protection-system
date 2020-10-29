8f25ba32e55c4cb4a119858ac454dc900ba72912
amit <47501611+AmitMizrahi123@users.noreply.github.com>
2020-10-29 12:50:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import face_recognition
 from server import db_collection, fs
 import numpy as np
 
-
+flag = False
 images_list = []
 names_list = []
 encoding_list = []
 
+
 for images in db_collection.find():
     name = images['username']
     image = images['images'][0]
-    g_out = fs.get(images['images'][0]['imageID'])
+    g_out = fs.get(image['imageID'])
     img = np.frombuffer(g_out.read(), dtype=np.uint8)
     img = np.reshape(img, image['shape'])
     images_list.append(img)
@@ -19,13 +20,12 @@ for images in db_collection.find():
     encode = face_recognition.face_encodings(img)[0]
     encoding_list.append(encode)
 
-
-cap = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(0)
 
 while True:
-    success, img = cap.read()
-    image_location = face_recognition.face_locations(img)
-    image_encoding = face_recognition.face_encodings(img, image_location)
+    success, frame = video_capture.read()
+    image_location = face_recognition.face_locations(frame)
+    image_encoding = face_recognition.face_encodings(frame, image_location)
 
     for encode_face, location_face in zip(image_encoding, image_location):
         matches = face_recognition.compare_faces(encoding_list, encode_face)
@@ -40,9 +40,17 @@ while True:
             color = (0, 255, 0)
 
         top, right, bottom, left = location_face
-        cv2.rectangle(img, (left, top), (right, bottom), color, 2)
-        cv2.rectangle(img, (left, bottom - 35), (right, bottom), color, cv2.FILLED)
-        cv2.putText(img, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_PLAIN, 1.3, text_color, 2)
+        cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), color, cv2.FILLED)
+        cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_PLAIN, 1.3, text_color, 2)
 
-    cv2.imshow('iamge', img)
-    cv2.waitKey(1)
+    cv2.imshow('iamge', frame)
+
+    if not flag:
+        flag = True
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
