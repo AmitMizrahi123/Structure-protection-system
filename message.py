30f31fde82f614721e4d2748e4ca35002b4f6f55
amit <47501611+AmitMizrahi123@users.noreply.github.com>
2020-10-30 09:53:38 +0200

    Update message.py

--- message.py
+++ message.py
@@ -1,16 +1,20 @@
 from twilio.rest import Client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def send_message(phone_list):
-    account_sid = '<ACCOUNT SID TWILIO>'
-    auth_token = '<AUTH TOKEN TWILIO>'
+    account_sid = os.getenv('ACCOUNT_SID')
+    auth_token = os.getenv('AUTH_TOKEN')
     client = Client(account_sid, auth_token)
 
     for phone in phone_list:
 
         message = client.messages.create(
-            body='<BODY MESSAGE>',
-            from_='<TWILIO NUMBER>',
+            body=os.getenv('BODY_MSG_TWILIO'),
+            from_=os.getenv('FROM_TWILIO'),
             to=phone
         )
 
