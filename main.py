e65d79a49db805f0ba86596c10b7856970aae981
amit <47501611+AmitMizrahi123@users.noreply.github.com>
2020-11-09 20:27:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,36 +13,40 @@ encoding_list = []
 
 for images in db_collection.find():
     name = images['username']
-    phone_number = images['phone_number'].replace('0', '+972', 1)  # Replace the number to israel number
+    phone_number = images['phone_number'].replace('0', 'whatsapp:+972', 1)
     image = images['images'][0]
     g_out = fs.get(image['imageID'])
     img = np.frombuffer(g_out.read(), dtype=np.uint8)
     img = np.reshape(img, image['shape'])
     images_list.append(img)
     names_list.append(name)
-    phone_list.append(phone_number)
+    if phone_number not in phone_list:
+        phone_list.append(phone_number)
     encode = face_recognition.face_encodings(img)[0]
     encoding_list.append(encode)
 
 video_capture = cv2.VideoCapture(0)
+
+video_capture.set(3, 640)
+video_capture.set(4, 480)
+video_capture.set(10, 300)
 
 while True:
     success, frame = video_capture.read()
     image_location = face_recognition.face_locations(frame)
     image_encoding = face_recognition.face_encodings(frame, image_location)
 
-    name = "Unknown Person"
-    color = (0, 0, 255)
-    text_color = (0, 0, 0)
-
     for encode_face, location_face in zip(image_encoding, image_location):
         matches = face_recognition.compare_faces(encoding_list, encode_face)
+
+        name = "Unknown Person"
+        color = (0, 0, 255)
+        text_color = (0, 0, 0)
 
         if True in matches:
             first_match_index = matches.index(True)
             name = names_list[first_match_index]
             color = (0, 255, 0)
-            flag = True
 
         top, right, bottom, left = location_face
         cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
@@ -52,8 +56,7 @@ while True:
     cv2.imshow('iamge', frame)
 
     if flag and name == "Unknown Person":
-        send_message(phone_list)
-        flag = False
+        flag = send_message(phone_list)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
